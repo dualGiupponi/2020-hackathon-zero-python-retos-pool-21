0e10c4d9c0129908c9b36ee6142cca73732c2539
Dual Giupponi <dual.giupponi@gmail.com>
2020-05-16 12:21:27 -0300

    Kata 1 Realizada

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -7,17 +7,28 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Ganaste!'
 #'Perdiste!'
 def quienGana(player, ai):
-    return ""
+    #Obtengo el elemento para hacer la comparación
+    elemPlayer = options.index(player.lower().capitalize())
+    elemAI = options.index(ai.lower().capitalize())
+    resultado = ""
+
+    #Caso de empate
+    if elemAI == elemPlayer: resultado = "Empate!"
+    #Casos que gana Player
+    if elemPlayer == 0 and elemAI == 2: resultado = 'Ganaste!' #Piedra vs tijera
+    if elemPlayer == 1 and elemAI == 0: resultado = 'Ganaste!' #Papel vs piedra
+    if elemPlayer == 2 and elemAI == 1: resultado = 'Ganaste!' #Tijera vs papel
+    
+    #En caso de que no sea ninguno
+    if resultado == "": resultado = 'Perdiste!'
+    
+    return resultado
 
 # Entry Point
 def Game():
-    #
-    #
-    
-    #
-    #
+    player = input()
+    ai = input()
     
     winner = quienGana(player, ai)
 
     print(winner)
-
