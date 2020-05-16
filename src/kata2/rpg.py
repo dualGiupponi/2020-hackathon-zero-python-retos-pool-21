2748085ac68c5beea902aaac2fe2d8958b0dabfc
Dual Giupponi <dual.giupponi@gmail.com>
2020-05-16 12:33:44 -0300

    Kata 2 Terminada

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,9 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+    resultado = ""
+    random.seed()
+    for i in range(0,passLen):
+        resultado += chr(random.randint(33, 126))
 
-    return ""
+    return resultado
